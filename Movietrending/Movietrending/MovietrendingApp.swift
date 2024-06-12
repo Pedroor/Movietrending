c505fe54f0fce858ce09c8bee4211b09be7f2c07
Pedro Andrade <parthur259@gmail.com>
2024-06-12 18:53:48 -0300

    feat: init structure

---
+++ Movietrending/Movietrending/MovietrendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovietrendingApp.swift
+//  Movietrending
+//
+//  Created by Pedro Andrade on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MovietrendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
